f6d198b2c2ba3e69995b41d9dfcf1c8318064e02
fadime94 <fadime.ozdemir11@gmail.com>
2020-09-17 17:08:13 +0300

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -11,6 +11,23 @@ class Timer extends Component {
   }
 
   //Your code here
+  //  shouldComponentUpdate(nextProp, nextState){
+  //   return !(this.state.time===nextState.time);
+  // }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time === nextState.time) {
+      return false
+    }
+    return true
+  }
+  
+  componentDidUpdate(){
+    this.timer.current.style.color ="#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+  componentDidUpdate(){
+    this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
 
   componentDidMount() {
     this.interval = setInterval(
